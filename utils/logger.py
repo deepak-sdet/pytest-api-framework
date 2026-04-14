fefd6a3d56874b009b1dfd969ac5791beafbaf93
Deepak Kumar <87904921+deepak-sdet@users.noreply.github.com>
2026-04-14 08:41:48 -0400

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+
+def get_logger():
+    logger = logging.getLogger("api_tests")
+
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+
+        console_handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    return logger
